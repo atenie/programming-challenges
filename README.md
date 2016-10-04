6c01ae745e93a6d9d33cc5abb986bcf2a519a887
Alexandru Tenie <alex_tenie@yahoo.com>
2016-10-04 14:48:08 +0300

    Bugfix + made it compliant with the standard Unix shell.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# tomato-pie: A bash shell Pomodoro timer script
+# tomato-pie: A *nix shell Pomodoro timer script
 
 ## Synopsis
-According to [Wikipedia](https://en.wikipedia.org/wiki/Pomodoro_Technique), the Pomodoro technique is a time management method. This is a bash shell script for tomato-loving time managers.
+According to [Wikipedia](https://en.wikipedia.org/wiki/Pomodoro_Technique), the Pomodoro technique is a time management method. This is a *nix shell script for tomato-loving time managers.
 
 ## Motivation
 I like to use the Pomodoro technique to practice some form of self-discipline. Now you can use it too.
 Good luck, working Padawan. Tomato pies await you.
 
 ## Installation and usage
-Copy it in `/usr/bin`, run `tomato-pie` in your terminal. Run `tomato-pie -d` if you want to use the default schedule with four 25 mins. work sessions with 5 mins. small breaks and a large 15 mins. break at the end.  Select `y` or `n` if you want to go again or not.
+Copy it in `/usr/bin`, run `tomato-pie` in your terminal. Run `tomato-pie -d` if you want to use the default schedule with four 25 mins. work sessions with 5 mins. small breaks and a large 15 mins. break at the end.  Select `y/Y` or `n/N` if you want to go again or not.
 
 ## Issues
-This project isn't yet POSIX-compatible, so if you want to run it in Debian/Ubuntu, expect stuff to not work.
+None, as far as I know. Please file bugs, if they appear.
 
 ## License
 Project created under the Simplified BSD License by Alexandru Tenie.
--- tomato-pie.sh
+++ tomato-pie.sh
@@ -1,75 +1,88 @@
 #!/bin/bash
 
-let ifdefault=0
+ifdefault=0
 
-while getopts ":d" opt; do
-    let work=1500
-    let wcopy=$work
-    let spause=300
-    let scopy=$spause
-    let bpause=900
-    let bcopy=$bpause
-    let number=4
-    let backupnumber=$number
-    let ifdefault=1
+while getopts ":d" opt
+do
+    work=1500
+    wcopy=1500
+
+    spause=300
+    scopy=300
+
+    bpause=900
+    bcopy=900
+
+    number=4
+    backupnumber=4
+
+    ifdefault=1
 done
 
 if [ $ifdefault -eq 0 ]
 then
     read -p "Insert work time (minutes): " work
-    let work=$work\*60
-    let wcopy=$work
+    ((wcopy=$work))
     read -p "Insert small pause time (minutes): " spause
-    let spause=$spause\*60
-    let scopy=$spause
+    ((scopy=$spause))
     read -p "Insert big pause time (minutes): " bpause
-    let bpause=$bpause\*60
-    let bcopy=$bpause
+    ((bcopy=$bpause))
     read -p "Insert number of small breaks: " number
-    let backupnumber=$number
+    ((backupnumber=$number))
 fi
-
-let number=$number-1
 
 while [ $backupnumber -ne 0 ]
 do
     while [ $number -ne 0 ]
     do
-        echo "Work time!"
-        while [ $work -gt 0 ]; do
+        echo "Work time!                "
+
+        while [ $work -gt 0 ]
+        do
             echo -ne "$(($work%3600/60)) minutes $(($work%60)) seconds\033[0K\r"
             sleep 1
-            : $((work--))
+            ((work=$work-1))
         done
-        let number=$number-1
-        echo "Take a slice of pie!"
-        while [ $spause -gt 0 ]; do
+        ((number=$number-1))
+
+        echo "Take a slice of pie!                  "
+
+        while [ $spause -gt 0 ]
+        do
             echo -ne "$(($spause%3600/60)) minutes $(($spause%60)) seconds\033[0K\r"
             sleep 1
-            : $((spause--))
+            ((spause=$spause-1))
         done
-        let work=$wcopy
-        let spause=$scopy
+        ((work=$wcopy))
+        ((spause=$scopy))
     done
-    echo "Work time!"
-    while [ $work -gt 0 ]; do
+
+    echo "Work time!                    "
+
+    while [ $work -gt 0 ]
+    do
         echo -ne "$(($work%3600/60)) minutes $(($work%60)) seconds\033[0K\r"
         sleep 1
-        : $((work--))
+        ((work=$work-1))
     done
-    echo "Take an entire pie now!"
-    while [ $bpause -gt 0 ]; do
+
+    echo "Take an entire pie now!                   "
+
+    while [ $bpause -gt 0 ]
+    do
         echo -ne "$(($bpause%3600/60)) minutes $(($bpause%60)) seconds\033[0K\r"
         sleep 1
-        : $((bpause--))
+        ((bpause=$bpause-1))
     done
+
     read -p "Do you want to go again? [y/n] " cond
-    if [ "$cond" = "y" ]
+    if [ "$cond" = "y" ] || [ "$cond" = "Y" ]
     then
-        let number=$backupnumber
-        let work=$wcopy
-        let spause=$scopy
-    elif [ "$cond" = "n" ]
+        ((number=$backupnumber))
+        ((work=$wcopy))
+        ((spause=$scopy))
+        ((bpause=$bcopy))
+    elif [ "$cond" = "n" ] || [ "$cond" = "N" ]
     then
         exit 0
     fi
